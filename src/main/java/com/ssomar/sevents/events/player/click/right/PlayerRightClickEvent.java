db94da8188a43f865303b68cb304b820a41a8653
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2021-11-26 16:35:48 +0100

    new getter

--- src/main/java/com/ssomar/sevents/events/player/click/right/PlayerRightClickEvent.java
+++ src/main/java/com/ssomar/sevents/events/player/click/right/PlayerRightClickEvent.java
@@ -24,6 +24,10 @@ public class PlayerRightClickEvent extends PlayerEvent implements Cancellable {
         this.block = block;
     }
 
+    public Block getBlock() {
+        return block;
+    }
+
     public boolean hasBlock() {return block != null;}
 
     @Override

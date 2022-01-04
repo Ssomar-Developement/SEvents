445e8c2108ca7c5fc0304d5102c931125d602a7a
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2022-01-04 21:48:40 +0100

    Add informative comment

--- src/main/java/com/ssomar/sevents/events/player/fish/nothing/PlayerFishBlobListener.java
+++ src/main/java/com/ssomar/sevents/events/player/fish/nothing/PlayerFishBlobListener.java
@@ -1,5 +1,6 @@
 package com.ssomar.sevents.events.player.fish.nothing;
 
+import com.ssomar.sevents.version.Version;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -12,7 +13,8 @@ public class PlayerFishBlobListener implements Listener {
     @EventHandler
     public void onPlayerFishEvent(PlayerFishEvent e) {
 
-        if (e.getState().equals(PlayerFishEvent.State.REEL_IN)) {
+        /* Not compatible in 1.12 and 1.13 */
+        if ((Version._1_14.is() || Version._1_15.is() || Version._1_16.is() || Version._1_17.is() || Version._1_18.is()) && e.getState().equals(PlayerFishEvent.State.REEL_IN)) {
             PlayerFishNothingEvent playerFishNothingEvent = new PlayerFishNothingEvent((Player) e.getPlayer(), e.getHook());
             Bukkit.getServer().getPluginManager().callEvent(playerFishNothingEvent);
             if (playerFishNothingEvent.isCancelled()) {

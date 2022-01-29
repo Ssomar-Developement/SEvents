6bf0040fcaed5c22cee550a5601d8aadeee031a1
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2022-01-29 19:33:27 +0100

    Update

--- src/main/java/com/ssomar/sevents/events/player/click/EntityDamageByEntityEventExtension.java
+++ src/main/java/com/ssomar/sevents/events/player/click/EntityDamageByEntityEventExtension.java
@@ -1,19 +1,22 @@
 package com.ssomar.sevents.events.player.click;
 
+import com.google.common.base.Function;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 /** Event creates to call it to damage something and avoid to have a loop of PlayerLeftClickOnEntity / Player **/
 public class EntityDamageByEntityEventExtension extends EntityDamageByEntityEvent {
 
     private boolean isAvoidLoop = false;
 
-    public EntityDamageByEntityEventExtension(@NotNull Entity damager, @NotNull Entity damagee, @NotNull EntityDamageEvent.DamageCause cause, double damage, boolean isAvoidLoop) {
-        super(damager, damagee, cause, damage);
-        this.isAvoidLoop = isAvoidLoop;
+    public EntityDamageByEntityEventExtension(@NotNull Entity damager, @NotNull Entity damagee, @NotNull EntityDamageEvent.DamageCause cause, @NotNull Map<DamageModifier, Double> modifiers, @NotNull Map<DamageModifier, ? extends Function<? super Double, Double>> modifierFunctions) {
+        super(damager, damagee, cause, modifiers, modifierFunctions);
     }
+
 
     public boolean isAvoidLoop() {
         return isAvoidLoop;
--- src/main/java/com/ssomar/sevents/events/player/click/onentity/left/PlayerLeftClickOnEntityListener.java
+++ src/main/java/com/ssomar/sevents/events/player/click/onentity/left/PlayerLeftClickOnEntityListener.java
@@ -1,31 +1,37 @@
 package com.ssomar.sevents.events.player.click.onentity.left;
 
 import com.ssomar.sevents.events.player.click.EntityDamageByEntityEventExtension;
-import com.ssomar.sevents.events.player.click.onplayer.left.PlayerLeftClickOnPlayerEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerLeftClickOnEntityListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent e) {
 
-        if (!(e instanceof EntityDamageByEntityEventExtension) || !((EntityDamageByEntityEventExtension) e).isAvoidLoop()) {
-            if (e.getDamager() instanceof Player) {
-                if (!(e.getEntity() instanceof Player)) {
-                    PlayerLeftClickOnEntityEvent playerLeftClickOnEntityEvent = new PlayerLeftClickOnEntityEvent((Player) e.getDamager(), e.getEntity());
-                    Bukkit.getServer().getPluginManager().callEvent(playerLeftClickOnEntityEvent);
-                    if (playerLeftClickOnEntityEvent.isCancelled()) {
-                        e.setCancelled(true);
-                    }
+        if (e instanceof EntityDamageByEntityEventExtension && ((EntityDamageByEntityEventExtension) e).isAvoidLoop()) {
+            if(e.getEntity() instanceof LivingEntity) {
+                LivingEntity ent =  (LivingEntity) e.getEntity();
+                ent.damage(e.getDamage());
+                ent.setLastDamage(e.getDamage());
+                ent.setLastDamageCause(e);
+            }
+            return;
+        }
+        if (e.getDamager() instanceof Player) {
+            if (!(e.getEntity() instanceof Player)) {
+                PlayerLeftClickOnEntityEvent playerLeftClickOnEntityEvent = new PlayerLeftClickOnEntityEvent((Player) e.getDamager(), e.getEntity());
+                Bukkit.getServer().getPluginManager().callEvent(playerLeftClickOnEntityEvent);
+                if (playerLeftClickOnEntityEvent.isCancelled()) {
+                    e.setCancelled(true);
                 }
             }
         }
+
     }
 }

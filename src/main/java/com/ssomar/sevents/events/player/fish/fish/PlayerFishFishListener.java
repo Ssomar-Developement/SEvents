bfbdfc588ddec88db8303c7c17f3d61e47bcdb42
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2022-06-19 21:10:18 +0200

    Update

--- src/main/java/com/ssomar/sevents/events/player/fish/fish/PlayerFishFishListener.java
+++ src/main/java/com/ssomar/sevents/events/player/fish/fish/PlayerFishFishListener.java
@@ -16,7 +16,7 @@ public class PlayerFishFishListener implements Listener {
         if (e.getState().equals(PlayerFishEvent.State.CAUGHT_FISH)) {
             PlayerFishFishEvent playerFishFishEvent = new PlayerFishFishEvent((Player) e.getPlayer(), e.getCaught());
             Bukkit.getServer().getPluginManager().callEvent(playerFishFishEvent);
-            if(playerFishFishEvent.getCaught().isEmpty()) e.getCaught().remove();
+            if(playerFishFishEvent.getCaught().isDead()) e.getCaught().remove();
             e.setExpToDrop(playerFishFishEvent.getAmount());
             if (playerFishFishEvent.isCancelled()) {
                 e.setCancelled(true);

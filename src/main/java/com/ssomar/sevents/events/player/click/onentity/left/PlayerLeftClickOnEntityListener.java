baa56d8e5556f5b064bfd1d359573698e1120545
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2022-09-28 08:34:40 +0200

    Update PlayerLeftClickOnEntityListener.java

--- src/main/java/com/ssomar/sevents/events/player/click/onentity/left/PlayerLeftClickOnEntityListener.java
+++ src/main/java/com/ssomar/sevents/events/player/click/onentity/left/PlayerLeftClickOnEntityListener.java
@@ -8,11 +8,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class PlayerLeftClickOnEntityListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent e) {
+
+        if(e.getCause().equals(EntityDamageEvent.DamageCause.THORNS)) return;
 
         if (e.getDamager() instanceof Player) {
             Player damager = (Player)e.getDamager();

83184c7e4bc5968a5a28d0d3a356ce1de64873c8
Marceau LEYMONERIE <marceau1607@gmail.com>
2023-09-04 21:26:57 +0200

    Update PlayerKillEntityEvent.java

--- src/main/java/com/ssomar/sevents/events/player/kill/entity/PlayerKillEntityEvent.java
+++ src/main/java/com/ssomar/sevents/events/player/kill/entity/PlayerKillEntityEvent.java
@@ -2,6 +2,7 @@ package com.ssomar.sevents.events.player.kill.entity;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.inventory.ItemStack;
@@ -9,10 +10,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class PlayerKillEntityEvent extends PlayerEvent {
+public class PlayerKillEntityEvent extends PlayerEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
-    private boolean cancel = false;
+    private boolean cancel;
     private final Entity entity;
     private int droppedExp;
     private List<ItemStack> drops;
@@ -26,6 +27,7 @@ public class PlayerKillEntityEvent extends PlayerEvent {
         this.entity = entity;
         this.droppedExp = droppedExp;
         this.drops = drops;
+        this.cancel = false;
     }
 
     public Entity getEntity() {
@@ -65,5 +67,15 @@ public class PlayerKillEntityEvent extends PlayerEvent {
 
     public void setDrops(List<ItemStack> drops) {
         this.drops = drops;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        cancel = b;
     }
 }

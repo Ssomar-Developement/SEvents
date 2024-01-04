1f469d3d0bfc2af64dd61e6ee4816d96ff4a2687
Marceau LEYMONERIE <marceau1607@gmail.com>
2024-01-04 18:39:48 +0100

    Adapt to make the entity left click activates even if damage = 0 for some entity like itemframe

--- src/main/java/com/ssomar/sevents/events/player/click/onentity/left/PlayerLeftClickOnEntityListener.java
+++ src/main/java/com/ssomar/sevents/events/player/click/onentity/left/PlayerLeftClickOnEntityListener.java
@@ -2,6 +2,7 @@ package com.ssomar.sevents.events.player.click.onentity.left;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Hanging;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,13 +11,31 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.hanging.HangingBreakByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PlayerLeftClickOnEntityListener implements Listener {
+
+    List<EntityType> entity0Damage;
+
+    public PlayerLeftClickOnEntityListener() {
+        entity0Damage = new ArrayList<>();
+        try{
+            entity0Damage.add(EntityType.ITEM_FRAME);
+        } catch (Exception | Error e) {}
+        try{
+            entity0Damage.add(EntityType.GLOW_ITEM_FRAME);
+        } catch (Exception | Error e) {}
+    }
+
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent e) {
 
-        if (e.getCause().equals(EntityDamageEvent.DamageCause.THORNS) || e.getDamage() == 0) return;
+        if (e.getCause().equals(EntityDamageEvent.DamageCause.THORNS) || (e.getDamage() == 0 && entity0Damage.contains(e.getEntity().getType()))) return;
 
         if (e.getDamager() instanceof Player) {
             Player damager = (Player) e.getDamager();
@@ -34,6 +53,7 @@ public class PlayerLeftClickOnEntityListener implements Listener {
 
     }
 
+    /* Destroy hanging , like itemframe */
     @EventHandler(priority = EventPriority.LOWEST)
     public void onHangingBreakByEntityEvent(HangingBreakByEntityEvent e) {
         Entity damager = e.getRemover();
